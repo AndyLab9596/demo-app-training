d29fda2702ddba1100b6aa0943b8c85abea6af15
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-08 20:32:54 +0700

    set up basis

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hook";
+import { decrement, increment, selectCount } from "./counterSlice";
+
+const Counter = () => {
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  return (
+    <Fragment>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </Fragment>
+  );
+};
+
+export default Counter;
