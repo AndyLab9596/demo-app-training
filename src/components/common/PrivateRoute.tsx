d5c02d1c71beb751410f06a6d4a828670740ce9d
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-09 08:46:58 +0700

    install MUI and config css baseline

--- src/components/common/PrivateRoute.tsx
+++ src/components/common/PrivateRoute.tsx
@@ -5,7 +5,7 @@ export interface PrivateProps {}
 
 export const PrivateRoute = (props: RouteProps) => {
   const isLoggedIn = localStorage.getItem('access_token');
-
+  console.log('logged into admin');
   if (!isLoggedIn) return <Redirect to="/login" />;
 
   return <Route {...props} />;
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <CssBaseline />
         <App />
       </BrowserRouter>
     </Provider>
