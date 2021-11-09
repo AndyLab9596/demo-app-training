cefa87d9f7e8fb51258c20bb118c6c63be159e47
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-09 15:26:01 +0700

    basis admin css

--- src/components/layout/Admin.tsx
+++ src/components/layout/Admin.tsx
@@ -1,8 +1,18 @@
-import { Box, Button } from '@material-ui/core';
-import React from 'react';
+import {
+  AppBar,
+  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from '@material-ui/core';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import React, { Fragment } from 'react';
 import { useAppDispatch } from '../../app/hook';
 import { authActions } from '../../features/auth/authSlice';
-
+import { Header, Main, Sidebar, Title, Wrapper, WrapperSidebar } from './Admin.style';
 export interface AdminLayoutProps {}
 
 export const AdminLayout = (props: AdminLayoutProps) => {
@@ -12,13 +22,39 @@ export const AdminLayout = (props: AdminLayoutProps) => {
     dispatch(authActions.logout());
   };
   return (
-    <div>
-      <Box m={2}>
-        <Button fullWidth variant="outlined" color="primary" onClick={handleLogout}>
-          Logout
-        </Button>
-      </Box>
-    </div>
+    <Fragment>
+      <Header>
+        <AppBar position="static">
+          <Toolbar>
+            <Title>Management</Title>
+            <Button color="secondary" onClick={handleLogout}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Header>
+      <Wrapper>
+        <Sidebar>
+          <WrapperSidebar>
+            <List component="nav" aria-label="main mailbox folders">
+              <ListItem button>
+                <ListItemIcon>
+                  <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <EmojiPeopleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Student" />
+              </ListItem>
+            </List>
+          </WrapperSidebar>
+        </Sidebar>
+        <Main></Main>
+      </Wrapper>
+    </Fragment>
   );
 };
 
