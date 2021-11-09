bc9d754adcb43bf02f5b5ed15546329e9749e5dc
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-09 09:02:02 +0700

    basis building login page UI

---
+++ src/features/auth/pages/LoginPage.styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+
+`
+
+export const LoginArea = styled.div`
+    width: 500px;
+    height: 200px;
+    border: 1px solid #000;
+    border-radius: 22px;
+    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+-webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+`
+
+export const Header = styled.div`
+    padding: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,27 @@
+import { Box, Button, Paper, Typography } from '@material-ui/core';
 import React from 'react';
+import { Header, LoginArea, Wrapper } from './LoginPage.styles';
 
 export interface LoginProps {}
 
 const LoginPage = (props: LoginProps) => {
-  return <div>Login page</div>;
+  return (
+    <Wrapper>
+      <LoginArea>
+        <Header>
+          <Typography variant="h5" component="h1" color="secondary" align="center">
+            Managements
+          </Typography>
+        </Header>
+
+        <Box m={4}>
+          <Button fullWidth variant="outlined" color="primary">
+            Login
+          </Button>
+        </Box>
+      </LoginArea>
+    </Wrapper>
+  );
 };
 
 export default LoginPage;
