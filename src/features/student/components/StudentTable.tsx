465db9262afed073d243a4a8f7260945cf0b5426
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-10 08:29:54 +0700

    done pagination

---
+++ src/features/student/components/StudentTable.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Student } from '../../../models';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from '@material-ui/core';
+
+export interface StudentTableProps {
+  studentList: Student[];
+  onEdit?: (student: Student) => void;
+  onRemove?: (student: Student) => void;
+}
+
+const StudentTable = ({ studentList, onEdit, onRemove }: StudentTableProps) => {
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Mark</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell align="center">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {studentList.map((student, idx) => (
+            <TableRow key={student.id}>
+              <TableCell>{student.id}</TableCell>
+              <TableCell>{student.name}</TableCell>
+              <TableCell>{student.gender}</TableCell>
+              <TableCell>{student.mark}</TableCell>
+              <TableCell>{student.city}</TableCell>
+              <TableCell align="center">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => onEdit?.(student)}
+                  style={{ marginRight: 8 }}
+                >
+                  Edit
+                </Button>
+                <Button variant="outlined" color="secondary" onClick={() => onRemove?.(student)}>
+                  Remove
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default StudentTable;
--- src/features/student/pages/ListPage.tsx
+++ src/features/student/pages/ListPage.tsx
@@ -1,19 +1,51 @@
+import { Box, Button, Typography } from '@material-ui/core';
 import React, { useEffect } from 'react';
-import { useAppDispatch } from '../../../app/hook';
-import { studentActions } from '../studentSlice';
+import { useAppDispatch, useAppSelector } from '../../../app/hook';
+import StudentTable from '../components/StudentTable';
+import {
+  selectStudentFilter,
+  selectStudentList,
+  selectStudentPagination,
+  studentActions,
+} from '../studentSlice';
+import Pagination from '@material-ui/lab/Pagination';
 
 const ListPage = () => {
   const dispatch = useAppDispatch();
+  const studentList = useAppSelector(selectStudentList);
+  const { _page, _limit, _totalRows } = useAppSelector(selectStudentPagination);
+  const filter = useAppSelector(selectStudentFilter);
   useEffect(() => {
-    dispatch(
-      studentActions.fetchStudentList({
-        _page: 1,
-        _limit: 15,
-      })
-    );
-  }, [dispatch]);
+    dispatch(studentActions.fetchStudentList(filter));
+  }, [dispatch, filter]);
 
-  return <div></div>;
+  console.log();
+
+  const handleChange = (e: any, page: number) => {
+    dispatch(studentActions.setFilter({ ...filter, _page: page }));
+  };
+
+  return (
+    <Box p={3}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography variant="h4">Students</Typography>
+        <Button variant="contained" color="primary">
+          Add Student
+        </Button>
+      </Box>
+      {/* Student table */}
+      <StudentTable studentList={studentList} />
+      {/* Pagination */}
+      <Box mt={2} display="flex" justifyContent="center">
+        <Pagination
+          count={Math.ceil(_totalRows / _limit)}
+          page={_page}
+          onChange={handleChange}
+          color="primary"
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default ListPage;
